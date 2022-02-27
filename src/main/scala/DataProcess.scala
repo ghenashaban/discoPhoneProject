35b23f07253bdd50557c73a2d52719c7ba84235d
Ghena.Shaban <ghena@hotmail.co.uk>
2022-02-27 21:46:35 +0000

    refactoring

--- src/main/scala/DataProcess.scala
+++ src/main/scala/DataProcess.scala
@@ -8,21 +8,24 @@ object DataProcess extends App {
     Source.getClass.getResource(s"/$fileName") match {
       case null => Left(Error("file not found"))
       case _ if Source.fromResource(fileName).nonEmpty => {
-        val callsLogsToList: List[String] = Source.fromResource(fileName).mkString.split("\\n").map(_.trim).toList
-        val parseCalls: Either[Error, List[Call]] = callsLogsToList.map(parseCall).sequence
-       printAll(parseCalls)
+        val callLogsToList: List[String] = Source.fromResource(fileName).mkString.split("\\n").map(_.trim).toList
+        val parseCalls: Either[Error, List[Call]] = callLogsToList.map(parseCall).sequence
+        printOutput(parseCalls)
         parseCalls
       }
       case _ => Left(Error("Empty File"))
     }
   }
 
-  def printAll(parseCalls :Either[Error, List[Call]] ) = {
-    val talk = toTalk(customerIdGroupedByTotalCost(parseCalls))
-    for (element <- talk.map(_.map(_.value)).getOrElse(None)) {
-      println(element)
-    }
+  def printOutput(parseCalls: Either[Error, List[Call]]) = parseCalls match {
+    case Right(_) =>
+      val talk: List[Talk] = toTalk(customerIdGroupedByTotalCost(parseCalls))
+      for (element <- talk) {
+        println(element.value)
+      }
+    case Left(_) => println("something went wrong")
   }
+
 
   def parseCall(string: String): Either[Error, Call] = {
     val splitLine: List[String] = string.split(" ").map(_.trim).toList
@@ -33,64 +36,64 @@ object DataProcess extends App {
     }
   }
 
-  def customerIdGroupedByTotalCost(callLogsList: Either[Error, List[Call]]): Either[Error, List[(CustomersId, Cost)]] = {
+  def customerIdGroupedByTotalCost(callLogsList: Either[Error, List[Call]]): List[(CustomersId, Cost)] = {
     callLogsList match {
       case Right(calls) => {
-        val test: Map[CustomersId, List[CallDuration]] = calls.groupBy(_.customersId).map {
-          case (key, value) => key -> value.map(_.callDuration)
+        val filteredCalls: List[Call] = promotionApplied(calls)
+        val joinCallWithCost: List[(CustomersId, Cost)] = joinCustomerWithCost(filteredCalls, listOfCosts(filteredCalls, List()))
+        val customerGroupedByCosts: Map[CustomersId, List[Cost]] = joinCallWithCost.groupBy(_._1).map {
+          case (key, value) => key -> value.map(value => Cost(value._2.value))
         }
-        val callDurationMappedByIdToLocalTime: Map[CustomersId, List[LocalTime]] = test.map { case (key, value) => key -> value.map(value => LocalTime.parse(value.value)) }
-        println(callDurationMappedByIdToLocalTime)
-        Right(filterCallsForPromotion(callDurationMappedByIdToLocalTime))
+        calculateTotalCost(customerGroupedByCosts)
       }
-      case Left(Error(_)) => Left(Error("Error in customerIdGroupedByTotalCost"))
     }
   }
 
-  def filterCallsForPromotion(callMappedById: Map[CustomersId, List[LocalTime]]): List[(CustomersId, Cost)] = {
-    val localTimeCallDurationList: List[List[LocalTime]] = callMappedById.values.toList
-    val callDurationToSecondsList: List[List[Long]] = localTimeCallDurationList.map(value => value.map(value => Duration.between(LocalTime.MIN, value).getSeconds))
-    println("call duration" + callDurationToSecondsList)
-//    val filteredCalls: List[List[Long]] = callDurationToSecondsList.map(value => value.map(value => value.value)).map(value => value.filter(_ != value.max))
-    val filteredCalls: List[List[Long]] = callDurationToSecondsList.map(value => value.filter(_ != value.max))
-   println(filteredCalls)
-    val filteredCallsDurationInSeconds: List[List[CallDurationInSeconds]] = filteredCalls.map(value => value.map(value => CallDurationInSeconds(value)))
-    println(filteredCallsDurationInSeconds)
-    joinCustomerWithCost(callMappedById.keys.toList, listOfCallCostsPerCustomer(filteredCalls, List()))
+  def calculateTotalCost(customerGroupedByCosts: Map[CustomersId, List[Cost]]): List[(CustomersId, Cost)] = {
+    val customerGroupedByTotalCost: List[(CustomersId, Cost)] = customerGroupedByCosts.toList.map {
+      case (key, value) => key -> Cost(Math.round(value.map(_.value).sum / 0.01)*0.01)
+    }
+    customerGroupedByTotalCost
   }
 
-  def listOfCallCostsPerCustomer(listOfCallDuration: List[List[Long]], listOfCalculatedCost: List[Cost]): List[Cost] = listOfCallDuration match {
+  def promotionApplied(list: List[Call]): List[Call] = {
+    val longDuration = list.map(value => value.callDuration.value).max
+    val filteredCalls = list.filter(_.callDuration.value != longDuration)
+    filteredCalls
+  }
+
+  def listOfCosts(listOfCalls: List[Call], listOfCalculatedCost: List[Cost]): List[Cost] = listOfCalls match {
     case a :: aq => {
       val callCost: Cost = calculateCost(a)
-      val total: List[Cost] = List(callCost) ++ listOfCallCostsPerCustomer(aq, listOfCalculatedCost)
+      val total: List[Cost] = List(callCost) ++ listOfCosts(aq, listOfCalculatedCost)
       total
     }
     case Nil => listOfCalculatedCost
   }
 
-  def calculateCost(listOfCallDuration: List[Long], totalCost: Cost = Cost(0)): Cost = listOfCallDuration match {
-    case a :: aq => {
-      val cost = if (a >= 180) Cost(0.05 * a) else Cost(0.03 * a)
-      calculateCost(aq, Cost(totalCost.value + cost.value))
-    }
-    case Nil => totalCost
+  def calculateCost(listOfCallDuration: Call): Cost = {
+    val durationInLong: Long = Duration.between(LocalTime.MIN, LocalTime.parse(listOfCallDuration.callDuration.value)).getSeconds
+    val cost = if (durationInLong >= 180) Cost(0.05 * durationInLong) else Cost(0.03 * durationInLong)
+    cost
+
   }
 
-  def joinCustomerWithCost(customerIdMappedWithDuration: List[CustomersId], listOfCallCostPerCustomer: List[Cost]): List[(CustomersId, Cost)] = {
-    val customerIdJoinedWithCost: List[(CustomersId, Cost)] = customerIdMappedWithDuration zip listOfCallCostPerCustomer
+  def joinCustomerWithCost(filteredCalls: List[Call], listOfCallCostPerCustomer: List[Cost]): List[(CustomersId, Cost)] = {
+    val customerIdJoinedWithCost = filteredCalls.map(_.customersId) zip listOfCallCostPerCustomer
     customerIdJoinedWithCost
   }
 
-  def toTalk(listOfCustomerWithCost: Either[Error, List[(CustomersId, Cost)]], talks: List[Talk] = List()): Either[Error, List[Talk]] = listOfCustomerWithCost match {
-    case Right(a :: aq) => {
+
+  def toTalk(listOfCustomerWithCost: List[(CustomersId, Cost)], talks: List[Talk] = List()): List[Talk] = listOfCustomerWithCost match {
+    case a :: aq => {
       val talk: Talk = Talk(s"${a._1} has a bill of £${a._2.value}")
-      val result = toTalk(Right(aq), List(talk) ++ talks)
+      val result = toTalk(aq, List(talk) ++ talks)
       result
     }
-    case Right(Nil) => Right(talks)
-    case _ => Left(Error("Some thing went wrong"))
+    case Nil => talks
   }
 
-  dataProcess("simple-calls.log")
+  dataProcess("calls.log")
+
 }
 
--- src/test/scala/PhoneTest.scala
+++ src/test/scala/PhoneTest.scala
@@ -1,70 +1,71 @@
-//import DataProcess._
-//import org.scalatest.flatspec.AnyFlatSpec
-//import java.time.LocalTime
-//import scala.collection.immutable.HashMap
-//
-//class PhoneProjectTest extends AnyFlatSpec {
-//
-//  val customerAfirstCall = Call(CustomersId("A"), PhoneNumberCalled("555-333-212"), CallDuration("00:02:03"))
-//  val customerAsecondCall = Call(CustomersId("A"), PhoneNumberCalled("555-433-242"), CallDuration("00:06:41"))
-//  val customerBfirstCall = Call(CustomersId("B"), PhoneNumberCalled("555-333-212"), CallDuration("00:01:20"))
-//
-//
-//  "Call log" should "return a list of Calls" in {
-//    val actualResult = dataProcess("simple-calls.log")
-//    val expectedResult = Right(List(customerAfirstCall, customerAsecondCall, customerBfirstCall))
-//    assertResult(expectedResult)(actualResult)
-//  }
-//
-//  "Call logs parsed correctly where a string of call" should "return a Call" in {
-//    val stringCallLog = "A 555-333-212 00:02:03"
-//    val actualResult = parseCall(stringCallLog)
-//    val expectedResult = Right(customerAfirstCall)
-//    assertResult(expectedResult)(actualResult)
-//  }
-//
-//  "List of Calls" should "return Customer grouped by call duration" in {
-//    val actualResult = customerIdGroupedByTotalCost(Right(List(customerAfirstCall, customerAsecondCall, customerBfirstCall)))
-//    val expectedResult = Right(List(
-//      CustomersId("A") -> Cost(3.69),
-//      CustomersId("B") -> Cost(0.0)))
-//    assertResult(expectedResult)(actualResult)
-//  }
-//
-//  "Customer with a list of call durations" should "return a list of filtered call durations in seconds" in {
-//    val actualResult = filterCallsForPromotion(Map(CustomersId("A") ->
-//      List(LocalTime.parse("00:02:03"),
-//        LocalTime.parse("00:06:41"),
-//        LocalTime.parse("00:01:20"))))
-//    val expectedResult = List(List(CallDurationInSeconds(123), CallDurationInSeconds(80)))
-//    assertResult(expectedResult)(actualResult)
-//  }
-//
-//  "Given a list of durations in seconds per customer" should "return total costs per customer after filter applied" in {
-//    val listOfCallDurationinSecondsFiltered = List(List(CallDurationInSeconds(123)), List(CallDurationInSeconds(80)))
-//    val actualResult = listOfCallCostsPerCustomer(listOfCallDurationinSecondsFiltered, List())
-//    val expectedResult: List[Cost] = List(Cost(3.69), Cost(2.4))
-//    assertResult(expectedResult)(actualResult)
-//  }
-//
-//  "CustomerA list of duration of calls in seconds" should "return total cost" in {
-//    val actualResult = calculateCost(List(CallDurationInSeconds(123), CallDurationInSeconds(63)))
-//    val expectedResult = Cost(5.58)
-//    assertResult(expectedResult)(actualResult)
-//  }
-//
-//  "Given List of customerId and a list of total cost per customer" should "join customerId with cost" in {
-//    val customerIdMappedWithDuration = List(CustomersId("A"), CustomersId("B"))
-//    val listOfCallCostPerCustomerFiltered = List(Cost(3.69), Cost(2.4))
-//    val actualResult = joinCustomerWithCost(customerIdMappedWithDuration, listOfCallCostPerCustomerFiltered)
-//    val expectedResult = List((CustomersId("A"), Cost(3.69)), (CustomersId("B"), Cost(2.4)))
-//    assertResult(expectedResult)(actualResult)
-//  }
-//
-//  "Given a customerId with total cost" should "return a string" in {
-//    val actualResult = DataProcess.toTalk(Right(List(
-//      (CustomersId("B"), Cost(28.64))))).getOrElse(None)
-//    val expectedResult = "CustomersId(B) has a bill of £28.64"
-//    assertResult(expectedResult)(actualResult)
-//  }
-//}
+import DataProcess._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class PhoneProjectTest extends AnyFlatSpec {
+
+  val customerAfirstCall = Call(CustomersId("A"), PhoneNumberCalled("555-333-212"), CallDuration("00:02:03"))
+  val customerAsecondCall = Call(CustomersId("A"), PhoneNumberCalled("555-433-242"), CallDuration("00:06:41"))
+  val customerBfirstCall = Call(CustomersId("B"), PhoneNumberCalled("555-333-212"), CallDuration("00:01:20"))
+  val customerBsecondCall = Call(CustomersId("B"), PhoneNumberCalled("555-333-212"), CallDuration("00:02:15"))
+
+  "Call log" should "return a list of Calls" in {
+    val actualResult = dataProcess("simple-calls.log")
+    val expectedResult = Right(List(customerAfirstCall, customerAsecondCall, customerBfirstCall, customerBsecondCall))
+    assertResult(expectedResult)(actualResult)
+  }
+
+  "Call logs parsed correctly where a string of call" should "return a Call item" in {
+    val stringCallLog = "A 555-333-212 00:02:03"
+    val actualResult = parseCall(stringCallLog)
+    val expectedResult = Right(customerAfirstCall)
+    assertResult(expectedResult)(actualResult)
+  }
+
+  "List of Calls" should "return Customer grouped by call duration" in {
+    val actualResult = customerIdGroupedByTotalCost(Right(List(customerAfirstCall, customerAsecondCall, customerBfirstCall)))
+    val expectedResult = List(
+      CustomersId("B") -> Cost(2.4),
+      CustomersId("A") -> Cost(3.69)
+    )
+    assertResult(expectedResult)(actualResult)
+  }
+
+  "Customer with a list of call durations" should "return a list of filtered call durations in seconds" in {
+    val actualResult = promotionApplied(List(customerAfirstCall, customerAsecondCall, customerBfirstCall, customerBsecondCall))
+    val expectedResult = List(customerAfirstCall, customerBfirstCall, customerBsecondCall)
+    assertResult(expectedResult)(actualResult)
+  }
+
+  "Given a Call Item" should "return cost for call" in {
+    val actualResult = calculateCost(customerAfirstCall)
+    val expectedResult = Cost(3.69)
+    assertResult(expectedResult)(actualResult)
+  }
+
+  "Given filtered calls" should "return list of costs per call" in {
+    val filteredCalls = List(customerAfirstCall, customerBfirstCall, customerBsecondCall)
+    val actualResult = listOfCosts(filteredCalls, List())
+    val expectedResult = List(Cost(3.69), Cost(2.4), Cost(4.05))
+    assertResult(expectedResult)(actualResult)
+  }
+
+  "Given CustomerId mapped to all call durations" should "return customerId with total cost" in {
+    val actualResult = calculateTotalCost(Map(CustomersId("B") -> List(Cost(2.4), Cost(4.05))))
+    val expectedResult = List((CustomersId("B"), Cost(6.45)))
+    assertResult(expectedResult)(actualResult)
+  }
+
+  "Given List of filtered calls and a list of total costs" should "join customerId with total cost" in {
+    val filteredCalls = promotionApplied(List(customerAfirstCall, customerAsecondCall, customerBfirstCall, customerBsecondCall))
+    val listOfCallCostPerCustomerFiltered = List(Cost(3.69), Cost(2.4))
+    val actualResult = joinCustomerWithCost(filteredCalls, listOfCallCostPerCustomerFiltered)
+    val expectedResult = List((CustomersId("A"), Cost(3.69)), (CustomersId("B"), Cost(2.4)))
+    assertResult(expectedResult)(actualResult)
+  }
+
+  "Given a customerId with total cost" should "return a string" in {
+    val actualResult = DataProcess.toTalk(List((CustomersId("A"), Cost(3.69)), (CustomersId("B"), Cost(6.45))))
+    val expectedResult = List(Talk("CustomersId(B) has a bill of £6.45"), Talk("CustomersId(A) has a bill of £3.69"))
+    assertResult(expectedResult)(actualResult)
+  }
+}
