8a34549c0838c87c013d74be9d5bf8d05b413124
Ghena.Shaban <ghena@hotmail.co.uk>
2022-03-28 11:02:45 +0100

    refactor

--- src/main/scala/service/Promotion.scala
+++ src/main/scala/service/Promotion.scala
@@ -10,20 +10,16 @@ trait Promotion {
 class ApplyPromotion extends Promotion {
 
   def applyPromotion(callWithCost: List[CallWithCost]): List[Cost] = {
-    val costListPerCustomer: List[Double] = callWithCost.groupBy(_.call.phoneNumberCalled).map{case(_, value) =>
-    value.map(_.cost.value).sum
-    }.toList
+
+    val costListPerCustomer = callWithCost.groupBy(_.call.phoneNumberCalled).view.mapValues(_.map(_.cost.value).sum).values.toList
 
     val listOfCostsPerCustomerFiltered = if (callWithCost.length > 1){
       costListPerCustomer.sorted.reverse.tail
     } else {
       costListPerCustomer
     }
-
     listOfCostsPerCustomerFiltered.map(Cost)
-
   }
-
 }
 
 
